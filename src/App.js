24f23eac8649330af6abc65eef7466beaee0296f
Raufun <raufun05@yahoo.com>
2020-11-27 18:37:56 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Container from "./components/container";
+import Navbar from "./components/header";
+import Footer from "./components/footer";
+import "./App.css";
+import Row from "./components/row";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Container>
+          <Row>
+            <h1 className="rounded col-sm-12 col-lg-3">Raufun Patoary</h1>
+            <Navbar/>
+            {/* use switch if you want to add "*" path */}
+            <Switch>
+              <Route exact path="/portfolio" component={Portfolio} />
+              <Route exact path="/contact" component={Contact} /> 
+              <Route exact path={["/", "/about", "*"]} component={About} />
+              </Switch>  
+          </Row>
+        </Container>
+      <Footer />
+    </Router>
   );
 }
 
 export default App;
+
